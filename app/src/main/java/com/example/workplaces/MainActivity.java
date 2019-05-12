92e0587a461ea7fe799e4a1193edc447a1a93de1
Ander0506 <andersonjesusujv@hotmail.com>
2019-05-11 21:52:15 -0500

    Creación del Login funcional

--- app/src/main/java/com/example/workplaces/MainActivity.java
+++ app/src/main/java/com/example/workplaces/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.workplaces;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText TxtUser, TxtPassword;
+    private FirebaseAuth Auth;
+    private Resources Res;
+
+    /* en el menu herramientas (firebase) escoger la opcion authentication
+     y conectar con un proyecto existente o crear uno nuevo y seguir los pasos
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TxtUser = (EditText) findViewById(R.id.TxtUser);
+        TxtPassword = (EditText) findViewById(R.id.TxtPassword);
+        Res = this.getResources();
+        Auth = FirebaseAuth.getInstance();
+
+    }
+
+    public void Login(View view){
+//        capturamos los valores de los campos en variables string para poder validar la informacion en firebase
+        String email = TxtUser.getText().toString();
+        String pass = TxtPassword.getText().toString();
+
+//        Verificación que los campos no se encuentren vacios
+        if(email.isEmpty() || pass.isEmpty()){
+            Toast toast = Toast.makeText(MainActivity.this, Res.getString(R.string.err_empty),Toast.LENGTH_LONG);
+            toast.show();
+            TxtUser.setError("*");
+            TxtPassword.setError("*");
+        }else{
+
+//            si los campos no se encuentran vacios ejecuta el codigo extraido de firebase
+
+            Auth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+
+//                                Si la autenticacion es valida procedemos a iniciar la siguiente pagina
+
+                                Intent intent = new Intent(MainActivity.this, Home.class);
+                                startActivity(intent);
+                            } else {
+
+//                                Si no mostramos un mensaje de error
+
+                                Toast toast = Toast.makeText(MainActivity.this, Res.getString(R.string.err_user),Toast.LENGTH_LONG);
+                                toast.show();
+                                TxtUser.setError("*");
+                                TxtPassword.setError("*");
+                            }
+
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/example/workplaces/Splash_Screen.java
+++ app/src/main/java/com/example/workplaces/Splash_Screen.java
@@ -1,9 +1,13 @@
 package com.example.workplaces;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,16 +16,32 @@ import android.widget.ImageView;
 public class Splash_Screen extends Activity {
 
     private ImageView imgLogo;
+    private ImageView imgLogo1;
+    private ImageView imgLogo2;
+    private ImageView imgLogo3;
     private Animation animLogo;
+    private Animation animLogo1;
+    private Animation animLogo2;
+    private Animation animLogo3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
 
-        imgLogo = (ImageView) findViewById(R.id.ImgViewLogo);
-        animLogo = AnimationUtils.loadAnimation(this, R.anim.animl_logo);
+        imgLogo = (ImageView) findViewById(R.id.imgViewLogo);
+        animLogo = AnimationUtils.loadAnimation(this, R.anim.anim_logo);
         imgLogo.setAnimation(animLogo);
+
+        imgLogo1 = (ImageView) findViewById(R.id.imgViewLogo1);
+        imgLogo2 = (ImageView) findViewById(R.id.imgViewLogo2);
+        imgLogo3 = (ImageView) findViewById(R.id.imgViewLogo3);
+        animLogo1 = AnimationUtils.loadAnimation(this, R.anim.anim_logo1);
+        animLogo2 = AnimationUtils.loadAnimation(this, R.anim.anim_logo2);
+        animLogo3 = AnimationUtils.loadAnimation(this, R.anim.anim_logo3);
+        imgLogo1.setAnimation(animLogo3);
+        imgLogo2.setAnimation(animLogo2);
+        imgLogo3.setAnimation(animLogo1);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -30,6 +50,6 @@ public class Splash_Screen extends Activity {
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
                 finish();
             }
-        },4000);
+        },5000);
     }
 }
